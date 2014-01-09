cba54cd1c6d34ca6d0ecaa71b8e7cd62e8121b2a
Modestas Stankus <stankus.modestas@gmail.com>
2014-01-09 22:44:04 +0200

    Few regexps for html parsing and nice test output

--- libmediateka.py
+++ libmediateka.py
@@ -3,21 +3,20 @@ import re
 MEDIATEKA_URL = 'http://www.lrt.lt/mediateka/'
 regexp = {
 	"pagrindinis": '<li ><a href="mediateka/(.*)">(.*)</a></li>',
-	"tiesiogiai": 'xxxx', 
+	"tiesiogiai": '<a class="channel\d*\s?\w*"\s*href="mediateka/(.*)">(.*)<\/a><\/li>',
 	"pasivyk": 'Pasivyk', 
 	"temos": '<li><a href="http://www.lrt.lt/mediateka/temos/(.*)>(.*)</a></li>',
 	"laidos": '<a href="mediateka/(.*)" style="float:left;">(.*)</a>',
 	"grojarasciai": 'Grojara\xc5\xa1\xc4\x8diai',
 	"prenumerata": 'Prenumerata',
 	"laidu-list": '<a class="name popup" title=\".*"\shref="mediateka/(.*)\?popup">(.*)</a>\s*<p\sclass="date">(.*)</p>',
-	"videolink": "type:\s*'html5'\,\s*config:\s*{\s*'file':\s*'(.*)',"
+	"videolink": "type:\s*'html5'\,\s*config:\s*{\s*'file':\s*'(.*)',",
+        "abecele": '\s*<li><a  href="mediateka/(laidos\/.)">(.)</a></li>'
 	}
-
-
 
 def get_page_html(link):
 	full_link = MEDIATEKA_URL+link
-	print(full_link)
+	print("->Parsing link %s" % full_link)
 	res = urllib.urlopen(full_link)
 	html = res.read()
 	res.close()
@@ -27,4 +26,3 @@ def get_data(link, regexp):
 	rexp = re.compile(regexp)
 	html = get_page_html(link)
 	return rexp.findall(html)
-
